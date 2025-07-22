adf84a51d7b1ca5a0fb8d9f79c9aba53f884db0a
Nicola <DALPOZZO92@GMAIL.COM>
2025-07-22 23:45:28 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/tdee-calculator/', // Aggiungi questa riga (sostituisci con il nome del tuo repository)
+
 })
